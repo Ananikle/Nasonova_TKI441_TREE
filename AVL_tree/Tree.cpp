0e0cd41491df601e51fdccaed13fd8849a2f6c4b
Анастасия Насонова <1146235@edu.rut-miit.ru>
2024-11-26 22:05:27 +0300

    Создание заготовки под реализацию класса Tree.

--- AVL_tree/Tree.cpp
+++ AVL_tree/Tree.cpp
@@ -1,3 +1,57 @@
 ﻿#include "Tree.h"
 
+//Конструктор с параметром для "Узла"
+Tree::Node::Node(int value)
+{
+	Node::value = value;
+	left = nullptr;
+	right = nullptr;
+}
 
+//Конструктор по умолчанию для "Дерева"
+Tree::Tree()
+{
+	root = nullptr;
+}
+
+//Конструктор копирования
+Tree::Tree(const Tree& other)
+{
+
+}
+
+//Деструктор
+Tree::~Tree()
+{
+
+}
+
+//Оператор присваивания
+Tree& Tree::operator=(const Tree& other)
+{
+	return *this;
+}
+
+//Вставка элемента в дерево
+void Tree::insert(int value)
+{
+
+}
+
+//Поиск элемента в дереве
+bool Tree::find(int value) const
+{
+	return false;
+}
+
+//Получение строкового предстваления дерева
+std::string Tree::toString() const
+{
+	return "";
+}
+
+//Оператор вывода в поток
+std::ostream& operator<<(std::ostream& out, const Tree& tree)
+{
+	return out;
+}
