bd162ca0b59de9d6a22f9ca1ab8932fe2d8200dc
Анастасия Насонова <1146235@edu.rut-miit.ru>
2024-12-02 12:24:46 +0300

    Добавили и реализовали проект Demo. Назначили его в качестве запускаемого проекта.

---
+++ Demo/Demo.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <iomanip>
+
+#include "../AVL_tree/Tree.cpp"
+
+int main()
+{
+	Tree t;
+
+	t.insert(5);
+	t.insert(8);
+	t.insert(15);
+	t.insert(1);
+	t.insert(-5);
+	t.insert(13);
+
+	std::cout << "|";
+	for (int i = -10; i <= 20; ++i)
+	{
+		std::cout << std::setw(4) << i << "|";
+	}
+	std::cout << std::endl;
+
+	std::cout << "|";
+	for (int i = -10; i <= 20; ++i)
+	{
+		std::cout << std::setw(4) << (t.find(i) ? "yes" : "-") << "|";
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
