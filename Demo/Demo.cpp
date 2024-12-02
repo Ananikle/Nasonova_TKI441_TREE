873c36c6e3ff3c0b818874207a6c7cc745c9b851
Анастасия Насонова <1146235@edu.rut-miit.ru>
2024-12-02 13:06:27 +0300

    Добавление стандартной функции проверки утечек памяти.

--- Demo/Demo.cpp
+++ Demo/Demo.cpp
@@ -3,30 +3,41 @@
 
 #include "../AVL_tree/Tree.cpp"
 
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
 int main()
 {
-	Tree t;
-
-	t.insert(5);
-	t.insert(8);
-	t.insert(15);
-	t.insert(1);
-	t.insert(-5);
-	t.insert(13);
-
-	std::cout << "|";
-	for (int i = -10; i <= 20; ++i)
+	//Блок, в котором "живёт" дерево
 	{
-		std::cout << std::setw(4) << i << "|";
-	}
-	std::cout << std::endl;
+		Tree t;
 
-	std::cout << "|";
-	for (int i = -10; i <= 20; ++i)
-	{
-		std::cout << std::setw(4) << (t.find(i) ? "yes" : "-") << "|";
+		t.insert(5);
+		t.insert(8);
+		t.insert(15);
+		t.insert(1);
+		t.insert(-5);
+		t.insert(13);
+
+		std::cout << "|";
+		for (int i = -10; i <= 20; ++i)
+		{
+			std::cout << std::setw(4) << i << "|";
+		}
+		std::cout << std::endl;
+
+		std::cout << "|";
+		for (int i = -10; i <= 20; ++i)
+		{
+			std::cout << std::setw(4) << (t.find(i) ? "yes" : "-") << "|";
+		}
+		std::cout << std::endl;
 	}
-	std::cout << std::endl;
+	//дерево перестало "жить"
+	
+	//Функция проверки утечки памяти (результат смотреть в MS VS в "Вывод", рядом со "Список ошибок")
+	_CrtDumpMemoryLeaks();
 
 	return 0;
 }
