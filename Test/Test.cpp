c33a995614abaf8ce2e2849aa1b0c5ac2bb70b9a
Анастасия Насонова <1146235@edu.rut-miit.ru>
2024-12-15 22:03:50 +0300

    Добавлен проект для тестирования.

---
+++ Test/Test.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "../AVL_tree/Tree.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Test
+{
+	TEST_CLASS(TreeTest)
+	{
+	public:
+		
+		TEST_METHOD(Method_ValidData_Success)
+		{
+
+		}
+	};
+}
