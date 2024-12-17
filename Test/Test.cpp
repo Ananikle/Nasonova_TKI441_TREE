579e0f95bd9284b5dec65a8bb2694a9e1d5507eb
Анастасия Насонова <1146235@edu.rut-miit.ru>
2024-12-17 14:03:17 +0300

    Добавлены заготовки для тестов методов классов Tree и Iterator.

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -11,7 +11,203 @@ namespace Test
 	{
 	public:
 		
-		TEST_METHOD(Method_ValidData_Success)
+		TEST_METHOD(DefaultConstructor_Success)
+		{
+
+		}
+
+		TEST_METHOD(Destructor_Success)
+		{
+
+		}
+
+		TEST_METHOD(CopyConstructor_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(MoveConstructor_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(CopyAssignmentOperator_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(CopyAssignmentOperator_InvalidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(MoveAssignmentOperator_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(MoveAssignmentOperator_InvalidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(Clear_Success)
+		{
+
+		}
+
+		TEST_METHOD(Insert_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(Insert_InvalidData_Success)
+		{
+
+		}
+
+
+		TEST_METHOD(Find_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(Find_InvalidData_Success)
+		{
+
+		}
+
+
+		TEST_METHOD(Remove_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(Remove_InvalidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(toString_Success)
+		{
+
+		}
+
+		TEST_METHOD(printAsTree_Success)
+		{
+
+		}
+
+		TEST_METHOD(Begin_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(Begin_InvalidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(Last_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(Last_InvalidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(End_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(End_InvalidData_Success)
+		{
+
+		}
+
+
+	};
+
+	TEST_CLASS(IteratorTest)
+	{
+	public:
+
+		TEST_METHOD(DefaultConstructor_Success)
+		{
+
+		}
+
+		TEST_METHOD(CopyConstructor_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(CopyAssignmentOperator_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(CopyAssignmentOperator_InvalidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(EqualOperator_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(NotEqualOperator_ValidData_Success)
+		{
+
+		}
+
+
+		TEST_METHOD(PrefixIncrementOperator_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(PrefixIncrementOperator_InvalidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(PostfixIncrementOperator_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(PostfixIncrementOperator_InvalidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(PrefixDecrementOperator_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(PrefixDecrementOperator_InvalidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(PostfixDecrementOperator_ValidData_Success)
+		{
+
+		}
+
+		TEST_METHOD(PostfixDecrementOperator_InvalidData_Success)
+		{
+
+		}
+
+
+		TEST_METHOD(DereferenceOperator_ValidData_Success)
 		{
 
 		}
