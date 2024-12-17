855f398626cfee791309f1df4504f36ed9fb91cb
Анастасия Насонова <1146235@edu.rut-miit.ru>
2024-12-17 13:31:55 +0300

    В демонстрационный проект добавлены перегрузки операторов ввода и вывода дерева, а также соотвтественно изменён интерфейс тестирования.

--- Demo/Demo.cpp
+++ Demo/Demo.cpp
@@ -7,6 +7,58 @@
 #include <stdlib.h>
 #include <crtdbg.h>
 
+//Вывод дерева в поток
+template <typename Type>
+std::ostream& operator<<(std::ostream& out, const Tree<Type>& tree)
+{
+	out << "TREE = ";
+
+	for (Tree<int>::Iterator it = t.begin(); it != t.last(); ++it)
+	{
+		out << *it << ", ";
+	}
+
+	if (t.last() != t.end())
+	{
+		out << *it << ". ";
+	}
+
+	return out;
+}
+
+//Ввод дерева из потока
+template <typename Type>
+std::istream& operator>>(std::istream& in, Tree<Type>& tree)
+{
+	tree.clear();
+
+	if (&in == &std::cin)
+	{
+		std::cout << "Enter how many values do you want to place in the tree?" << std::endl;
+	}
+	int count;
+	in >> count;
+
+	if (&in == &std::cin)
+	{
+		std::cout << "Enter values:" << std::endl;
+	}
+
+	for (int i = 0; i < count; ++i)
+	{
+		Type value;
+		in >> value;
+		bool isInserted = tree.insert(value);
+
+		if (&in == &std::cin)
+		{
+			std::cout << (isInserted ? "Added!" : "Failed!") << std::endl;
+		}
+	}
+
+	return in;
+}
+
 int main()
 {
 	//Блок, в котором "живёт" дерево
@@ -18,9 +70,11 @@ int main()
 		std::cout << "i <number> - insert number to the tree" << std::endl;
 		std::cout << "f <number> - find number in the tree" << std::endl;
 		std::cout << "d <number> - delete number from the tree" << std::endl;
+		std::cout << "r - read" << std::endl;
+		std::cout << "t - tree-print" << std::endl;
 		std::cout << "p - print" << std::endl;
-		std::cout << "t - check iterators" << std::endl;
-		std::cout << "e - exit" << std::endl;
+		std::cout << "e - check iterators" << std::endl;
+		std::cout << "x - exit" << std::endl;
 		std::cout << std::endl;
 
 		char choice;
@@ -53,12 +107,20 @@ int main()
 				std::cout << std::left << ": " << (t.remove(number) ? "SUCCEED" : "FAILED") << std::endl;
 				break;
 
-			case 'p':
-				std::cout << t << std::endl;
-				std::cout << "[" << t.toString() << "]" << std::endl;
+			case 'r':
+				std::cin >> t;
 				break;
 
 			case 't':
+				t.printAsTree();
+				std::cout << "[" << t.toString() << "]" << std::endl;
+				break;
+
+			case 'p':
+				std::cout << t << std::endl;
+				break;
+
+			case 'e':
 				for (Tree<int>::Iterator it = t.begin(); it != t.end(); ++it)
 				{
 					std::cout << *it << " > ";
@@ -72,7 +134,7 @@ int main()
 				std::cout << "END" << std::endl;
 				break;
 
-			case 'e':
+			case 'x':
 				break;
 
 			default:
